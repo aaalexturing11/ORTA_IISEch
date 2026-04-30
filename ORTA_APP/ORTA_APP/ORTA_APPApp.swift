c2b76dcc6b8c257967b9b3ae3f62702b6e61197e
Alexis Aguirre <alexisaguirre2811@gmail.com>
2026-04-30 01:08:51 -0600

    App vacía

---
+++ ORTA_APP/ORTA_APP/ORTA_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ORTA_APPApp.swift
+//  ORTA_APP
+//
+//  Created by Alexis Aguirre Alanís on 30/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ORTA_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
